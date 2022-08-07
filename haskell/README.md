20034e26ade297a14c0a4b0513fd46a4181e4b55
weirdev <weirdev@outlook.com>
2022-08-07 17:24:33 -0400

    Initial work on validation pass

--- haskell/README.md
+++ haskell/README.md
@@ -2,6 +2,7 @@ Well typed implementation of ideas I wrote in black FB notebook.
 
 Next steps:
 1. Iotas integration with eval
+    Instead doing a separate validation pass
 2. Proofs for iotas
 3. Proof transformation
 4. Test against motivating example cases (parallel iteration of lists, provably safe doubly linked list)
--- haskell/test.hs
+++ haskell/test.hs
@@ -1,32 +1,70 @@
-import Data.Map
+import           Data.Map                       ( Map
+                                                , delete
+                                                , empty
+                                                , insert
+                                                , lookup
+                                                )
 
-data Proofs = Prfs [Bool] deriving Show
-data Iota = I String deriving Show
+data Proofs = Prfs [Bool]
+    deriving Show
+type Iota = String
 type Variable = String
-data Value = VInt Integer deriving Show
+data Value = VInt Integer
+    deriving Show
 data Op = Plus | Minus
-data Expression = V Value | E Expression Op Expression
+data Expression = Val Value | Var Variable | E Expression Op Expression
 data Statement = Assign Variable Expression
 type State = (Map Variable Value, Map Variable Iota, Proofs)
+type VState = (Map Variable Iota, Proofs, [String])
 
--- Public fn
+iotalist :: [String]
+iotalist = [ l : show x | x <- [0 ..], l <- ['a' .. 'z'] ]
+
+-- Public fns
 evaluate :: [Statement] -> State
 evaluate [] = (empty, empty, Prfs [])
-evaluate l = evalProgram (evaluate []) l
+evaluate l  = evalProgram (empty, empty, Prfs []) l
+
+validate :: [Statement] -> VState
+validate [] = (empty, Prfs [], [head iotalist])
+validate l  = valProgram (empty, Prfs [], [head iotalist]) l
 
 -- Private fns
 evalProgram :: State -> [Statement] -> State
-evalProgram state [] = state
-evalProgram state (s : tail) = evalProgram (evalStatement state s) tail
+evalProgram = foldl evalStatement
+
+valProgram :: VState -> [Statement] -> VState
+valProgram = foldl valStatement
 
 evalStatement :: State -> Statement -> State
-evalStatement (vals, iotas, proofs) (Assign var expr) = 
-    (insert var (evalExpression (vals, iotas, proofs) expr) vals, delete var iotas, proofs)
+evalStatement (vals, iotas, proofs) (Assign var expr) =
+    let (val, miota) = evalExpression (vals, iotas, proofs) expr
+    in  case miota of
+            Just niota -> (insert var val vals, insert var niota iotas, proofs)
+            Nothing    -> (insert var val vals, delete var iotas, proofs)
 
-evalExpression :: State -> Expression -> Value
-evalExpression state (V val) = val
-evalExpression state (E expr1 op expr2) = evalOp op (evalExpression state expr1) (evalExpression state expr2)
+valStatement :: VState -> Statement -> VState
+valStatement (iotas, proofs, iotaseq) (Assign var expr) =
+    let niota : tiotalist = iotaseq
+    in  let nproofs = valExpression (iotas, proofs, iotaseq) expr
+        in  (insert var niota iotas, proofs, tiotalist)
+
+evalExpression :: State -> Expression -> (Value, Maybe Iota)
+evalExpression state (Val val) = (val, Nothing)
+evalExpression (vals, iotas, _) (Var var) =
+    let mval = Data.Map.lookup var vals
+    in  let miota = Data.Map.lookup var iotas
+        in  case mval of
+                Just val -> (val, miota)
+                Nothing  -> error "Undefined variable" -- TODO: Validate
+evalExpression state (E expr1 op expr2) =
+    let (val1, _) = evalExpression state expr1
+    in  let (val2, _) = evalExpression state expr2
+        in  (evalOp op val1 val2, Nothing)
+
+valExpression :: VState -> Expression -> Proofs
+
 
 evalOp :: Op -> Value -> Value -> Value
-evalOp Plus (VInt i1) (VInt i2) = VInt (i1 + i2)
+evalOp Plus  (VInt i1) (VInt i2) = VInt (i1 + i2)
 evalOp Minus (VInt i1) (VInt i2) = VInt (i1 - i2)
