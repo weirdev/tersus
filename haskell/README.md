ed6742633930b9ba2aec37a6554dc4153e1a4af6
weirdev <weirdev@outlook.com>
2022-08-07 23:29:30 -0400

    Basic validation!

--- haskell/README.md
+++ haskell/README.md
@@ -1,8 +1,10 @@
 Well typed implementation of ideas I wrote in black FB notebook.
 
 Next steps:
-1. Iotas integration with eval
-    Instead doing a separate validation pass
-2. Proofs for iotas
+1. Property objects
+2. Functions
+    Replace operators (plus, minus)
+3. Improved proof representation, proof engine
+    Proof engine would be able to apply generalized rewritings supplied by functions
 3. Proof transformation
 4. Test against motivating example cases (parallel iteration of lists, provably safe doubly linked list)
--- haskell/test.hs
+++ haskell/test.hs
@@ -5,8 +5,8 @@ import           Data.Map                       ( Map
                                                 , lookup
                                                 )
 
-data Proofs = Prfs [Bool]
-    deriving Show
+data Rel = Eq | Lt | Gt | LtEq | GtEq deriving (Eq, Show)
+data Proof = A Iota Rel Iota | C Iota Rel Value deriving Show -- This will need to be made more robust, for now A=abstract, C=concrete
 type Iota = String
 type Variable = String
 data Value = VInt Integer
@@ -14,20 +14,44 @@ data Value = VInt Integer
 data Op = Plus | Minus
 data Expression = Val Value | Var Variable | E Expression Op Expression
 data Statement = Assign Variable Expression
-type State = (Map Variable Value, Map Variable Iota, Proofs)
-type VState = (Map Variable Iota, Proofs, [String])
+type State = (Map Variable Value, Map Variable Iota, [Proof])
+type VState = (Map Variable Iota, [Proof], [String])
 
 iotalist :: [String]
 iotalist = [ l : show x | x <- [0 ..], l <- ['a' .. 'z'] ]
 
+proofLIotaLookup :: [Proof] -> Iota -> [Proof]
+proofLIotaLookup proofs iota = filter (proofLIota iota) proofs
+
+proofLIota :: Iota -> Proof -> Bool
+proofLIota iota (A piota _ _) = piota == iota
+proofLIota iota (C piota _ _) = piota == iota
+
+proofRel :: Rel -> Proof -> Bool
+proofRel rel (A _ prel _) = prel == rel
+proofRel rel (C _ prel _) = prel == rel
+
+proofConcrete :: Proof -> Bool
+proofConcrete C{} = True
+proofConcrete _   = False
+
+replaceLIotas :: [Proof] -> Iota -> [Proof]
+replaceLIotas [] _ = []
+replaceLIotas (A piota rel ptiota : tail) iota =
+    A iota rel ptiota : replaceLIotas tail iota
+replaceLIotas (C piota rel val : tail) iota =
+    C iota rel val : replaceLIotas tail iota
+
 -- Public fns
 evaluate :: [Statement] -> State
-evaluate [] = (empty, empty, Prfs [])
-evaluate l  = evalProgram (empty, empty, Prfs []) l
+evaluate [] = (empty, empty, [])
+evaluate l  = evalProgram (empty, empty, []) l
 
 validate :: [Statement] -> VState
-validate [] = (empty, Prfs [], [head iotalist])
-validate l  = valProgram (empty, Prfs [], [head iotalist]) l
+validate [] = (empty, [], [head iotalist])
+validate l =
+    let (iotas, proofs, remainingIotas) = valProgram (empty, [], iotalist) l
+    in  (iotas, proofs, [head remainingIotas])
 
 -- Private fns
 evalProgram :: State -> [Statement] -> State
@@ -46,8 +70,8 @@ evalStatement (vals, iotas, proofs) (Assign var expr) =
 valStatement :: VState -> Statement -> VState
 valStatement (iotas, proofs, iotaseq) (Assign var expr) =
     let niota : tiotalist = iotaseq
-    in  let nproofs = valExpression (iotas, proofs, iotaseq) expr
-        in  (insert var niota iotas, proofs, tiotalist)
+    in  let nproofs = valExpression (iotas, proofs, iotaseq) niota expr
+        in  (insert var niota iotas, proofs ++ nproofs, tiotalist)
 
 evalExpression :: State -> Expression -> (Value, Maybe Iota)
 evalExpression state (Val val) = (val, Nothing)
@@ -62,9 +86,36 @@ evalExpression state (E expr1 op expr2) =
     in  let (val2, _) = evalExpression state expr2
         in  (evalOp op val1 val2, Nothing)
 
-valExpression :: VState -> Expression -> Proofs
-
+valExpression :: VState -> Iota -> Expression -> [Proof] -- produces only the new proofs
+valExpression state iota (Val val) = [C iota Eq val]
+    -- foldr (\iota _ -> [C iota Eq val]) [] miota
+valExpression (iotas, proofs, _) iota (Var var) =
+    let omiota = Data.Map.lookup var iotas
+    in  case omiota of
+            Nothing    -> error "Undefined variable"
+            Just oiota -> [A iota Eq oiota]
+valExpression (iotas, proofs, iotaseq) iota (E expr1 op expr2) =
+    let niota1 : niota2 : tiotalist = iotaseq
+    in  let proofs1 = valExpression (iotas, proofs, iotaseq) niota1 expr1
+        in  let proofs2 = valExpression (iotas, proofs, iotaseq) niota2 expr2
+            in  valOp op niota1 proofs1 niota2 proofs2 iota
 
 evalOp :: Op -> Value -> Value -> Value
 evalOp Plus  (VInt i1) (VInt i2) = VInt (i1 + i2)
 evalOp Minus (VInt i1) (VInt i2) = VInt (i1 - i2)
+
+-- Takes: Op, left arg iota, Proofs of left arg, right arg iota, Proofs of right arg, result iota
+-- -- Returns: Proofs for result iota
+valOp :: Op -> Iota -> [Proof] -> Iota -> [Proof] -> Iota -> [Proof]
+valOp op liota lproofs riota rproofs retiota =
+    let lEqProofs = filter (proofRel Eq) (filter (proofLIota liota) lproofs)
+    in  let rEqProofs = filter
+                proofConcrete
+                (filter (proofRel Eq) (filter (proofLIota riota) rproofs))
+        in  let opfun = case op of
+                    Plus  -> (+)
+                    Minus -> (-)
+            in  case (lEqProofs, rEqProofs) of
+                    (C _ Eq (VInt li) : _, C _ Eq (VInt ri) : _) ->
+                        [C retiota Eq (VInt (opfun li ri))]
+                    _ -> error "Op not validated"
