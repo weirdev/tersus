323c1e0f6d254753b9c6e409eef0ed23fff62ae5
weirdev <weirdev@outlook.com>
2024-06-08 07:46:28 -0400

    All success cases for validate

--- tersus/test/TestTersus.hs
+++ tersus/test/TestTersus.hs
@@ -1,7 +1,6 @@
 module TestTersus where
 
-import Data.List (delete)
-import Data.Map (Map, findWithDefault, fromList, insert, insertWith, lookup)
+import Data.Map (Map, fromList, lookup)
 
 import Parse
 import Proof
@@ -19,7 +18,7 @@ type VarAssignedProof = Proof (Either Iota String)
 -- Core Test helper functions
 
 testCaseSeq :: String -> [TestResult] -> Test
-testCaseSeq s results = TestList s (zipWith (\i r -> TestCase (s ++ show i) r) [0 ..] results)
+testCaseSeq s results = TestList s (zipWith (\i r -> TestCase (s ++ show i) r) [0 :: Integer ..] results)
 
 runTest :: Test -> IO ()
 runTest (TestCase s r) =
@@ -42,7 +41,7 @@ testAssertEq actual expected =
         else Just $ "Expected: " ++ show expected ++ "\nGot: " ++ show actual
 
 testAllTrue :: (Show a) => (a -> Bool) -> [a] -> TestResult
-testAllTrue f [] = Nothing
+testAllTrue _ [] = Nothing
 testAllTrue f (x : xs) =
     if f x
         then testAllTrue f xs
@@ -90,7 +89,7 @@ expectedProofCompare (CTerm v1) (CTerm v2) _ = v1 == v2
 expectedProofCompare (ATerm var) (ATerm iota2) varMap = case Data.Map.lookup var varMap of
     Just iota1 -> iota1 == iota2
     Nothing -> False
-expectedProofCompare (FApp2 f1 p1) (FApp2 f2 p2) varMap = f1 == f2 && all (\(p1, p2) -> expectedProofCompare p1 p2 varMap) (zip p1 p2)
+expectedProofCompare (FApp2 f1 ps1) (FApp2 f2 ps2) varMap = f1 == f2 && all (\(p1, p2) -> expectedProofCompare p1 p2 varMap) (zip ps1 ps2)
 expectedProofCompare _ _ _ = False
 
 validateFCHelper :: [Statement] -> [VariableProof] -> TestResult
@@ -98,15 +97,20 @@ validateFCHelper stmts expected =
     let (varMap, iproofs, _) = validate stmts
      in testAllTrue (\vp -> expectedProofMatch vp iproofs varMap) expected
 
-testValidateFullContext :: Test
-testValidateFullContext =
+testValidateFullContextSuccesses :: Test
+testValidateFullContextSuccesses =
     testCaseSeq
-        "testValidateFullContext"
+        "testValidateFullContextSuccess"
         [ validateFCHelper [Assign "x" (F Size [Val (VIntList [5])])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 1)]]
+        , validateFCHelper [Assign "x" (Val (VIntList [5, 4])), Assign "y" (F Size [Var "x"])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VIntList [5, 4])], FApp2 (Rel Eq) [ATerm "y", CTerm (VInt 2)]]
+        , validateFCHelper [Assign "x" (F Size [Val (VIntList [5])]), Assign "y" (F Minus [Val (VInt 1), Val (VInt 1)])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 1)], FApp2 (Rel Eq) [ATerm "y", CTerm (VInt 0)]]
+        , validateFCHelper [Assign "x" (Val (VInt 5)), ProofAssert (FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 5)])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 5)]]
+        , validateFCHelper [Assign "x" (Val (VInt 5)), Rewrite (EqToLtPlus1 "x"), ProofAssert (FApp2 (Rel Lt) [ATerm "x", CTerm (VInt 6)])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 5)], FApp2 (Rel Lt) [ATerm "x", CTerm (VInt 6)]]
+        , validateFCHelper [Assign "x" (Val (VInt 5)), AssignProofVar "a" (Val (VInt 5)), Rewrite (Refl "x"), ProofAssert (FApp2 (Rel Eq) [ATerm "x", ATerm "a"])] [FApp2 (Rel Eq) [ATerm "x", CTerm (VInt 5)], FApp2 (Rel Eq) [ATerm "a", CTerm (VInt 5)], FApp2 (Rel Eq) [ATerm "x", ATerm "a"], FApp2 (Rel Eq) [ATerm "a", ATerm "x"]]
         ]
 
 main :: IO ()
 main = do
     runTest testParse
     runTest testEvaluateFullContext
-    runTest testValidateFullContext
+    runTest testValidateFullContextSuccesses
